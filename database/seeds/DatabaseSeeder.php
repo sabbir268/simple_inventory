064abc44d000b4213bb85c29d477beb482aba73d
unknown <sabbir.hossain2668@gmail.com>
2019-05-01 16:54:19 +0600

    demo admin&supplier generate

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Products;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,22 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         factory(Products::class,20)->create();
+
+        // create admin
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12345678'),
+            'role' => 'admin'
+        ]);
+
+        // create supplier
+        DB::table('users')->insert([
+            'name' => 'supplier',
+            'email' => 'supplier@gmail.com',
+            'password' => bcrypt('12345678'),
+            'role' => 'supplier'
+        ]);
+
     }
 }
